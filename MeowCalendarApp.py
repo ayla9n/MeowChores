e121ab5671cefab25e9a6fdc19e44f9129572dd5
Aala <ayla1719@gmail.com>
2025-10-26 16:13:44 -0400

    added sage photo

--- MeowCalendarApp.py
+++ MeowCalendarApp.py
@@ -47,6 +47,7 @@ today_person = calendar.get_person_for_date(today)
 tomorrow_person = calendar.get_person_for_date(tomorrow)
 
 if today_person:
+    st.write(f"\n{today.strftime("%A %B %d, %Y")}")
     st.subheader(f"⭐ Today is {today_person}'s Idda day!")
 else:
     st.warning("No one assigned for today.")
@@ -58,6 +59,7 @@ if tomorrow_person:
 info_col, img_col = st.columns(2)
 catwashing = Image.open("cat pics/justwashingthedishes.jpg")
 catmop = Image.open("cat pics/meowmop.jpg")
+sage = Image.open("cat pics/valsage.jpg")
 
 day_df = calendar.get_day_df()
 tomorrow_index = calendar.get_index_for_person(tomorrow_person)
@@ -67,21 +69,6 @@ with info_col:
     st.dataframe(day_df.set_index("Day").iloc[tomorrow_index:tomorrow_index + len(siblings) - 1], use_container_width=True)
     st.divider()
 
-    # Talaja Rotation
-    talaja = ["powderblue 🟦", "AYla 🐱", "Pixie 🧚🏾", "Acai 🫐"]
-    last_to_clean = 1
-    next_up = talaja[last_to_clean + 1:] + talaja[:last_to_clean]
-    st.subheader("❄️ Talaja Day")
-    st.info(f"{talaja[last_to_clean]} was the last person to clean the fridge")
-    st.dataframe(pd.DataFrame({'Next up': next_up}), use_container_width=True)
-
-with img_col:
-    st.image(catwashing, caption="you in a little bit")
-
-st.divider()
-
-col1, col2 = st.columns(2)
-with col1:
     st.subheader("Kitchen Reminders")
     st.markdown("""
     - 🍽️ Place clean plates on the dish rack  
@@ -90,6 +77,22 @@ with col1:
     - 🧹 Sweep the kitchen floor  
     - 🚮 Take out the garbage if it's full  
     """)
+
+with img_col:
+    st.image(sage, caption="you rn", width=250)
+    st.image(catwashing, caption="you in a little bit", width=250)
+
+st.divider()
+
+col1, col2 = st.columns(2)
+with col1:
+     # Talaja Rotation
+    talaja = ["powderblue 🟦", "AYla 🐱", "Pixie 🧚🏾", "Acai 🫐"]
+    last_to_clean = 1
+    next_up = talaja[last_to_clean + 1:] + talaja[:last_to_clean]
+    st.subheader("❄️ Talaja Day")
+    st.info(f"{talaja[last_to_clean]} was the last person to clean the fridge")
+    st.dataframe(pd.DataFrame({'Next up': next_up}), use_container_width=True)
 
 with col2:
     st.image(catmop, caption="also you in a little bit")
